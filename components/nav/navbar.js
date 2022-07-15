bacbb589be8af55836cd21e73ea5c0be024305fb
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-07-15 23:31:46 +0500

    starting working on card components

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
-import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from './navbar.module.css';
 
@@ -30,7 +31,14 @@ const NavBar = ({ username }) => {
 			<div className={styles.wrapper}>
 				<Link href='/'>
 					<a className={styles.logoLink}>
-						<div className={styles.logoWrapper}>Netflix</div>
+						<div className={styles.logoWrapper}>
+							<Image
+								src={'/static/netflix.svg'}
+								alt='Netflix logo'
+								width='128px'
+								height='34px'
+							/>
+						</div>
 					</a>
 				</Link>
 
@@ -45,9 +53,14 @@ const NavBar = ({ username }) => {
 
 				<nav className={styles.navContainer}>
 					<div>
-						<button className={styles.userNameBtn} onClick={handleShowDropdown}>
+						<button className={styles.usernameBtn} onClick={handleShowDropdown}>
 							<p className={styles.username}>{username}</p>
-							{/* Expand more icon */}
+							<Image
+								src={'/static/expand_more.svg'}
+								alt='Expand dropdown'
+								width='24px'
+								height='24px'
+							/>
 						</button>
 						{showDropdown && (
 							<div className={styles.navDropdown}>
