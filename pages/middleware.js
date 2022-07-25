0d0af9792acb69fcdb3f98eb08ac81f0d9136095
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-07-25 21:58:16 +0500

    deployment changes #3

--- pages/middleware.js
+++ pages/middleware.js
@@ -1,12 +1,7 @@
 import { NextResponse } from 'next/server';
 import { verifyToken } from '../lib/utils';
-import { spawn } from 'child_process';
 
 export default async function middleware(req, ev) {
-	if (process.version) {
-		spawn('ls', ['-lh']);
-	}
-
 	const token = req ? req.cookies.token : null;
 	const userId = verifyToken(token);
 
